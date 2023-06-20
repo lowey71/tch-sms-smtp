bbdf18e46e3bfd5138b89379ab210a01a543dd53
lowey71 <milo@loweys.net>
2023-06-20 10:37:22 +1000

    Update smtp.lua

--- smtp.lua
+++ smtp.lua
@@ -4,18 +4,20 @@ local smtp = require("socket.smtp")
 if ( arg[1] == nil or arg[2] == nil or arg[3] == nil or arg[4] == nil or arg[5] == nil ) then
   print ("ERROR - need 5 arguments - server from to subject body")
   os.exit(1)
-end  
-   
+end
+
 mesgt = {
   headers = {
-    subject = arg[4]
+    to = arg[3],
+    subject = arg[4],
+    from = arg[2]
   },
   body = arg[5]
 }
 
 r, e = smtp.send{
   from = arg[2],
-  rcpt = arg[3], 
+  rcpt = '<' .. arg[3] .. '>',
   source = smtp.message(mesgt),
   server = arg[1]
 }
